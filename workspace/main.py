afda63c4e5a89a049f66bbdb3cd4e92bab7ba870
EchoX466 <Roscoe.harmon@gmail.com>
2026-06-11 14:19:03 -0700

    4. Update main.py | Added word count functionality

--- workspace/main.py
+++ workspace/main.py
@@ -1,9 +1,15 @@
 def get_book_text():
-	with open("books/frankenstein.txt") as f:
-		print(f.read())
+  with open("books/frankenstein.txt") as f:
+    print(f.read())
+
+def word_count():
+  with open("books/frankenstein.txt") as e:
+    book = e.read()
+    num_words = len(book.split())
+    print(f"Found {num_words} total words")
 
 def main():
-	get_book_text()
+  get_book_text()
 
 if __name__ == "__main__":
-	main()
+  main()
