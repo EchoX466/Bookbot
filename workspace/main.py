dd9ab4711dd73afd331e6699a88c9d18bfba6c60
EchoX466 <Roscoe.harmon@gmail.com>
2026-06-12 09:09:03 -0700

    9. Updated main.py with a visualization of the word & character counts

--- workspace/main.py
+++ workspace/main.py
@@ -9,14 +9,34 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
-    print(f"Found {num_words} total words")
 
   # Call the function from stats.py to build a dictionary of characters and counts
     chars_dict = get_chars_dict(text)
-    
 
   # Convert the dictionary to a list and sort
     sorted_chars = chars_dict_to_sorted_list(chars_dict)
-    print(sorted_chars)
+
+    print_report(book_path, num_words, sorted_chars)
+
+def print_report(book_path, num_words, sorted_chars):
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+
+    for char, count in sorted_chars:
+        if not char.isalpha():
+            continue
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
+
+
+
+
 if __name__ == "__main__":
     main()
